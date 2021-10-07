6866dac8ff87744c830ecf84d5e983e20dbad7a9
benedict <kiokobenedict@gmail.com>
2021-10-07 20:28:57 +0300

    complete integration

---
+++ flask/app/views.py
@@ -0,0 +1,11 @@
+from app import app
+import os
+
+@app.route("/")
+def index():
+
+    # Use os.getenv("key") to get environment variables
+    app_name = os.getenv("APP_NAME")
+
+    if app_name:
+        return {"greetings": "Hello from Flask!"}
